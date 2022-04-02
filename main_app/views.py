e0daf39a8b3ed8d73379167e512c9bbd1e198778
skim121 <skim29121@gmail.com>
2022-04-02 12:50:25 -0500

    Basic setup complete

---
+++ main_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views import View 
+from django.views.generic import DetailView
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import DeleteView, CreateView, UpdateView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse 
+from .models import Cheese, Wine 
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator 
+
+class Home(TemplateView):
+    template_name = "home.html"
