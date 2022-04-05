275f17a5e7fc0cc69aaf69538682c2f3bdff3c8b
skim121 <skim29121@gmail.com>
2022-04-04 23:26:34 -0500

    User model started

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,29 @@
 from django.db import models
+from django.contrib.auth.models improt AbstractBaseUser, BaseUserManager
+
+class User(AbstractBaseUser): 
+    email = models.EmailField(verbose_name = "email", max_length=250, unique = True) 
+    fullname = models.CharField(max_length = 250)
+    shelter_admin = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['fullname']
+
+    def __str__(self): 
+        return self.fullname
+    
+    def has_perm(self, perm, obj=None): 
+        return self.is_admin
+
+    def has_module_perms(self, app_label): 
+        return True
+    
+
+
 
 KILL_CHOICES = (
     ("kill", "kill"),
