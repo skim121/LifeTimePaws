d620edf4dd8af3127ca94da084b32a3603ca215e
skim121 <skim29121@gmail.com>
2022-04-05 23:14:21 -0500

    Custom user model auth and admin site

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,53 @@
 from django.db import models
-from django.contrib.auth.models improt AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+#This is repeatitive on purpose - there is a shorter way to write this (with base function and other functions to pass in data) but it is not advisable to pass in password information directly. Passing in password would make it bypass encryption and make the user account less secure.
+class MyUserManager(BaseUserManager): 
+    def create_user(self, email, fullname, shelter_admin, password=None): 
+        if not email: 
+            raise ValueError("Users must enter an email address")
+        if not fullname: 
+            raise ValueError("Users must enter a name")
+        user = self.model(
+            email=self.normalize_email(email),
+            fullname = fullname, 
+            shelter_admin = False,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_shelter_user(self, email, fullname, shelter_admin, password=None): 
+        if not email: 
+            raise ValueError("Users must enter an email address")
+        if not fullname: 
+            raise ValueError("Users must enter a name")
+        user = self.model(
+            email=self.normalize_email(email),
+            fullname = fullname, 
+            shelter_admin = True,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+        
+    def create_superuser(self, email, fullname, shelter_admin, is_admin, is_staff, is_superuser, password=None): 
+        if not email: 
+            raise ValueError("Users must enter an email address")
+        if not fullname: 
+            raise ValueError("Users must enter a name")
+        user = self.model(
+            email=self.normalize_email(email),
+            fullname = fullname, 
+            shelter_admin = True,
+            is_admin = True, 
+            is_staff = True,
+            is_superuser = True,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
 class User(AbstractBaseUser): 
     email = models.EmailField(verbose_name = "email", max_length=250, unique = True) 
     fullname = models.CharField(max_length = 250)
@@ -10,8 +57,10 @@ class User(AbstractBaseUser):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
+    objects = MyUserManager()
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['fullname']
+    REQUIRED_FIELDS = ['fullname', 'shelter_admin', 'is_admin', 'is_staff', 'is_superuser']
 
     def __str__(self): 
         return self.fullname
@@ -22,7 +71,6 @@ class User(AbstractBaseUser):
     def has_module_perms(self, app_label): 
         return True
     
-
 
 
 KILL_CHOICES = (
